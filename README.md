054de4ed0858a6f24cce5ca9c76c30a3d6b7ee94
bandari5 <bsalotaibi1@gmail.com>
2019-12-04 22:54:24 +0300

    game

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Connect4Game
 
-
 - Connect Four (also known as Captain's Mistress, Four Up, Plot Four, Find Four, Four in a Row, Four in a Line, Drop Four, and Gravitrips.
 - the objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs. 
 
+# Technical part
 
+i used jquery for my codes.
 
-URL file:///Users/ban/Documents/GA/Connect4Game/index.html
+# Link
+
+ https://bandari5.github.io/Connect4Game/
--- project.js
+++ project.js
@@ -1,68 +1,56 @@
-window.onload = function(){ 
-     var turn = 0;
+   var turn = 0;
           var arr1=[13,9,5,1]
 $(document).ready(function(){  
          
-     
          $('.array1').on('click', function (event) { 
              if (turn==0)
-       {  
+       {          
            
-               
-          
         $("#game"+arr1[0]).addClass("green");
-           console.log(this)
+//           console.log(this)
            arr1.shift()
             turn=1;
             check();
-       }
-             
-            else if  (turn == 1)
-               { $("#game"+arr1[0]).addClass("blue");
+       }   
+           else if  (turn == 1)
+         { $("#game"+arr1[0]).addClass("blue");
                arr1.shift()
                 turn=0;
-                 check();
-                
-               }
-       
+                 check(); 
+             }
      });
      let arr2=[14,10,6,2]
      
          $('.array2').on('click', function (event) { 
-            console.log(arr2)
+//            console.log(arr2)
              if (turn==0)
        {  
-           
-               
-          
+                      
         $("#game"+arr2[0]).addClass("green");
-           console.log(this)
+//           console.log(this)
            arr2.shift()
           turn=1;
             check();
        }
              
-            else if  (turn == 1)
+               else if  (turn == 1)
                { $("#game"+arr2[0]).addClass("blue");
                arr2.shift()
                 turn=0;
                  check();
                 
                }
-       
      });
     
        let arr3=[15,11,7,3]
      
          $('.array3').on('click', function (event) { 
-            console.log(arr3)
+//            console.log(arr3)
              if (turn==0)
        {  
-           
-               
-          
+                
         $("#game"+arr3[0]).addClass("green");
-           console.log(this)
+//           console.log(this)
            arr3.shift()
           turn=1;
             check();
@@ -73,22 +61,18 @@ $(document).ready(function(){
                arr3.shift()
                 turn=0;
                  check();
-                
                }
-       
      });
     
        let arr4=[16,12,8,4]
      
          $('.array4').on('click', function (event) { 
-            console.log(arr4)
+//            console.log(arr4)
              if (turn==0)
        {  
-           
-               
-          
+             
         $("#game"+arr4[0]).addClass("green");
-           console.log(this)
+//           console.log(this)
            arr4.shift()
           turn=1;
             check();
@@ -98,14 +82,11 @@ $(document).ready(function(){
                { $("#game"+arr4[0]).addClass("blue");
                arr4.shift()
                 turn=0;
-                 check();
-                
+                 check();  
                }
-       
      });
         });
 
-    
 let check=function(){ 
 
          for (i=1 ; i<=4 ; i++){
@@ -117,14 +98,11 @@ let check=function(){
                     $("#game"+(i+8)).hasClass("green")&&
                     $("#game"+(i+12)).hasClass("green")){
                     
-                      alert("Green wins!!")
-                     $('.game').off("click");
-                }
-                    
-             
+                    alert("Green wins!!")
+                    $('.game').off("click");
+                }         
          //check blue
              
-  
                if ($("#game"+i).hasClass("blue")&&
                     $("#game"+(i+4)).hasClass("blue")&& 
                     $("#game"+(i+8)).hasClass("blue")&& 
@@ -133,12 +111,8 @@ let check=function(){
                    alert("Blue wins!!")
                    $('.game').off("click");
                }
-            
-             
               }
-        
        // row green
-             
              
               for (r=1 ; r<=13 ; r=r+4){
                    if ($("#game"+r).hasClass("green")&&
@@ -146,11 +120,9 @@ let check=function(){
                     $("#game"+(r+2)).hasClass("green")&&
                     $("#game"+(r+3)).hasClass("green")){
                      
-                        alert("Green wins!!")
-                        $('.game').off("click");
-                   }
-                   
-                  
+                    alert("Green wins!!")
+                     $('.game').off("click");
+                   }       
           // row blue
                   
                   if ($("#game"+r).hasClass("blue")&&
@@ -158,43 +130,40 @@ let check=function(){
                     $("#game"+(r+2)).hasClass("blue")&&
                     $("#game"+(r+3)).hasClass("blue")){
                       
-                       alert("Blue wins!!")
-                       $('.game').off("click");
-                  }
-                   
-                  
-         }
+                     alert("Blue wins!!")
+                     $('.game').off("click");
+                  }   
+              }
     
-    //x 
+           //x1
     
    if ($("#game1").hasClass("green")&&
                     $("#game6").hasClass("green")&&
                     $("#game11").hasClass("green")&&
                     $("#game16").hasClass("green")){
                      
-                        alert("Green wins!!")
-        $('.game').off("click");
+                     alert("Green wins!!")
+                     $('.game').off("click");
                    }
-    
     
      if ($("#game1").hasClass("blue")&&
                     $("#game6").hasClass("blue")&&
                     $("#game11").hasClass("blue")&&
                     $("#game16").hasClass("blue")){
                      
-                        alert("Blue wins!!")
-          $('.game').off("click");
+                     alert("Blue wins!!")
+                     $('.game').off("click");
                    }
     
-   //x2
+          //x2
     
     if ($("#game4").hasClass("green")&&
                     $("#game7").hasClass("green")&&
                     $("#game10").hasClass("green")&&
                     $("#game13").hasClass("green")){
                      
-                        alert("Green wins!!")
-         $('.game').off("click");
+                    alert("Green wins!!")
+                    $('.game').off("click");
                    }
     
     if ($("#game4").hasClass("blue")&&
@@ -202,20 +171,14 @@ let check=function(){
                     $("#game10").hasClass("blue")&&
                     $("#game13").hasClass("blue")){
                      
-                        alert("Blue wins!!")
-         $('.game').off("click");
+                    alert("Blue wins!!")
+                    $('.game').off("click");
     }
     
-     // tie
+          // tie
     else if ($(".game").not(".green").length==8 && $(".game").not(".blue").length==8){
             alert("It's a tie")
          $('.game').off("click");
     }
     
-}
-
-      
-    //here function ends
-
-       
 }
